60987af258cb4cda3020f1386a90e6e88200562b
s-gahira <s-gahira@53S500036644.it.bsd405.org>
2018-09-19 14:58:40 -0700

    Here is my finished project.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,86 @@
  */
 
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+	
+	public static void backSlashes1() {
+		System.out.println("|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+	public static void backSlashes2() {
+		System.out.println("||||||||||||||||||||||||||||||");
+	}
+	
+	public static void verticalLines1() {
+		System.out.print("||");
+	}
+	public static void verticalLines2() {
+		System.out.println("                           |");
+	}
+	
+	public static void vertLines3() {
+		System.out.println("|");
+	}
+	public static void bigSpace() {
+		vertLineFinal();
+		vertLineFinal();
+		vertLineFinal();
+		verticalLines1();
+		System.out.print("      ASCII Rules!!!       ");
+		vertLines3();
+		vertLineFinal();
+		vertLineFinal();
+		
+	}
+	
+	public static void vertLineFinal() {
+		verticalLines1();
+		verticalLines2();
+		
+	}
+	public static void keyboardWire () {
+		System.out.println("|_/        \\               \\_|\n+           \\                +\n            / \n            \\");
+	}
+	public static void keyboard() {
+		dashes();
+		upperRow();
+		midRow();
+		bottomRow();
+		System.out.print("   ");
+		dashes2();
+		System.out.println("\t\t\t\t  \\");
+		}
+	public static void dashes() {
+		System.out.println("-------------------------");
+	}
+	public static void dashes2() {
+		System.out.println("-------------------------      /");
+	}
+	public static void upperRow() {
+		System.out.println("\\  Q W E R T Y U I O P  \\");
+	}
+	public static void midRow() {
+		System.out.println(" \\   A S D F G H J K L   \\");
+	}
+	public static void bottomRow() {
+		System.out.println("  \\ Alt Z X C V B N M Ctrl\\------\\_");
+	}
+	public static void mouse() {
+		System.out.println("\t\t\t\t ____\n\t\t\t\t//||\\\\ \n\t\t\t\t||--|| \n\t\t\t\t\\\\--// \n\t\t\t\t \\\\//");
+	}
+	public static void main(String[] args) {
+		backSlashes1();
+		backSlashes2();
+		bigSpace();
+		System.out.println("||===========================|");
+		System.out.println("||Knockoff Dell Monitor 9000=|");
+		System.out.println("||===========================|");
+		keyboardWire();
+		keyboard();
+		mouse();
+		System.out.println("\n\nA computer depicting how ASCII and AP Comp Sci both rule.");
+		
+		
+		
+		
+	}
+}
